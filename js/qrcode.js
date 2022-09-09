576173f79ed771039edad220d47708adab381e29
Augusto <auguzsto@gmail.com>
2022-09-09 13:37:31 -0300

    Update

--- js/qrcode.js
+++ js/qrcode.js
@@ -1,7 +1,7 @@
 const url = localStorage.getItem('url')
 const info = localStorage.getItem('info')
-document.getElementById('qrcode').innerHTML = "<img src=https://chart.googleapis.com/chart?chs=450x450&cht=qr&chl="+url+">"
+document.getElementById('qrcode').innerHTML = "<img src=https://chart.googleapis.com/chart?chs=450x450&cht=qr&chl="+url+" class=img-fluid>"
 setTimeout(function(){
     window.location.reload(1);
  }, 1000);
- document.getElementById('info').innerHTML = "Valor inserido no QRCode R$"+info
+document.getElementById('info').innerHTML = "Valor inserido no QRCode R$"+info
